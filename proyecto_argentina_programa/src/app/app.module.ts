7b262f2f48f1de281178f18c11e67704b715dae2
Nicole Guillamon <nicoleguillamon@gmail.com>
2022-05-20 19:42:46 -0300

    add components skills, proyects, footer

--- proyecto_argentina_programa/src/app/app.module.ts
+++ proyecto_argentina_programa/src/app/app.module.ts
@@ -10,7 +10,10 @@ import { BannerComponent } from './components/banner/banner.component';
 import { AboutComponent } from './components/about/about.component';
 import { ExperienceComponent } from './components/experience/experience.component';
 import { EducationComponent } from './components/education/education.component';
-
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProyectsComponent } from './components/proyects/proyects.component';
+import { FooterComponent } from './components/footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,11 +23,31 @@ import { EducationComponent } from './components/education/education.component';
     BannerComponent,
     AboutComponent,
     ExperienceComponent,
-    EducationComponent
+    EducationComponent,
+    SkillsComponent,
+    ProyectsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({
+      "radius": 60,
+      "space": -10,
+      "outerStrokeGradient": true,
+      "outerStrokeWidth": 10,
+      "outerStrokeColor": "#609d9f",
+      "outerStrokeGradientStopColor": "#72b3f3",
+      "innerStrokeColor": "#e7e8ea",
+      "innerStrokeWidth": 10,
+      "animateTitle": false,
+      "animationDuration": 300,
+      "showUnits": false,
+      "showBackground": false,
+      "showZeroOuterStroke": false,
+      "showSubtitle": false,
+      "lazy": false
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
